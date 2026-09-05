27b5ef180b2fde5cea9dff06efc5b93ce537bee5
Farhad <toons0animotica@gmail.com>
2026-09-05 21:18:46 +0600

    Update Flutter channel to stable and adjust compileSdkVersion in build files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.virusdownloader"
-    compileSdk = 36
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
